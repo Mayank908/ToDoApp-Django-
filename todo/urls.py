eab1fb2e3110532c59f8d53751d876d7b2c5d26c
Mayank_007 <Mayank_007@github.com>
2023-10-20 00:35:41 +0530

    MarkasUndonefeature

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add Task
     path('addTask/', views.addTask, name='addTask'),
+
+    # MArk as Done
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
 
 ]
